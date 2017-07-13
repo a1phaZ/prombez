35a4ceab6b214111f77ef71b5138aec78f8c2288
a1phaZ <pride.ots@gmail.com>
2017-07-13 13:24:20 +0500

    deploy 2 heroku

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.session({cookie: {maxAge: 60000}}));
 app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
